2373d537ba74485148c45a101f7b72f4cd4a88da
godwu69 <hoangntth2306017@fpt.edu.vn>
2023-10-05 10:58:51 +0700

    update

---
+++ WebDesign/05.10.2023/js/script.js
@@ -0,0 +1,12 @@
+onload = function(){
+    init();
+}
+// DOM: Document object model
+function init(){
+    document.getElementById("magic").onmouseover = function(){
+        this.className = "highlight";
+    }
+    document.getElementById("magic").onmouseout = function(){
+        this.className = "";
+    }
+}
