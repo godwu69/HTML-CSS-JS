7f64e5e56ec90665cae40f9bb3d049327e85e4b8
godwu69 <hoangntth2306017@fpt.edu.vn>
2023-10-05 14:51:14 +0700

    update

---
+++ 05.10.2023/js/script3.js
@@ -0,0 +1,14 @@
+var msg = "Hello world";
+helloFunction(); // Gọi hàm
+
+function helloFunction(){
+    let i = 10;
+    const f = 55.5;
+    var x = 2000;
+    console.log(msg + "Viet Nam");
+    console.log(f, ++i);   
+}
+let msg = 'hello'
+console.log(msg);
+let weekdays = ["sun","mon","tue","wed","thu","fri"];
+console.log(weekdays);
